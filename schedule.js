a7d842793c6be7e728115bceda3e37943dc24846
Sid Roberts <sid@sidroberts.co.uk>
2021-02-20 19:15:07 +0900

    Added Palm Tree's schedule

--- schedule.js
+++ schedule.js
@@ -1,68 +1,131 @@
 /* global $, date */
 
-const schedule = [
-  // Monday
-  [
-    'Chinese',
-    'Classic',
-    'iSpring',
-    'Reading Project',
-    'Number Adventure',
-    'Discover and Explore'
+const schedules = {
+  'poplar': [
+    // Monday
+    [
+      'Chinese',
+      'Classic',
+      'iSpring',
+      'Reading Project',
+      'Number Adventure',
+      'Discover and Explore'
+    ],
+
+    // Tuesday
+    [
+      'Wonders',
+      'iSpring',
+      'iSpring',
+      'Body Movement',
+      'Story Art',
+      'Society and Me'
+    ],
+
+    // Wednesday
+    [
+      'Wonders',
+      'iSpring',
+      'Wonders',
+      'Reading Project',
+      'Body Movement',
+      'Classic'
+    ],
+
+    // Thursday
+    [
+      'Wonders',
+      'iSpring',
+      'iSpring',
+      'Discover and Explore',
+      'Society and Me',
+      'iGenius'
+    ],
+
+    // Friday
+    [
+      'Wonders',
+      'iSpring',
+      'Wonders',
+      'Reading Project',
+      'Number Adventure',
+      'iPlay'
+    ]
   ],
 
-  // Tuesday
-  [
-    'Wonders',
-    'iSpring',
-    'iSpring',
-    'Body Movement',
-    'Story Art',
-    'Society and Me'
-  ],
 
-  // Wednesday
-  [
-    'Wonders',
-    'iSpring',
-    'Wonders',
-    'Reading Project',
-    'Body Movement',
-    'Classic'
-  ],
 
-  // Thursday
-  [
-    'Wonders',
-    'iSpring',
-    'iSpring',
-    'Discover and Explore',
-    'Society and Me',
-    'iGenius'
-  ],
+  'palm': [
+    // Monday
+    [
+      'Wonders',
+      'iSpring',
+      'Chinese',
+      'Reading Project',
+      'Classic',
+      'Number Adventure',
+    ],
 
-  // Friday
-  [
-    'Wonders',
-    'iSpring',
-    'Wonders',
-    'Reading Project',
-    'Number Adventure',
-    'iPlay'
+    // Tuesday
+    [
+      'Wonders',
+      'iSpring',
+      'Reading Project',
+      'Body Movement',
+      'Word and Sentence',
+      'Discover and Explore'
+    ],
+
+    // Wednesday
+    [
+      'Wonders',
+      'Classic',
+      'iSpring',
+      'iSpring',
+      'Word and Sentence',
+      'iPlay'
+    ],
+
+    // Thursday
+    [
+      'Wonders',
+      'iSpring',
+      'Body Movement',
+      'Creative Writing',
+      'Society and Me',
+      'iGenius'
+    ],
+
+    // Friday
+    [
+      'Wonders',
+      'iSpring',
+      'Reading Project',
+      'Number Adventure',
+      'Discover and Explore',
+      'Society and Explore'
+    ]
   ]
-]
+}
 
 // Lessons that require a pencil at the very beginning of the class
 const pencilLessons = [
   'iGenius',
   'iSpring',
   'Wonders',
-  'Word and Sentence'
+  'Word and Sentence',
+  'Creative Writing'
 ]
 
 // Ignore weekends (Saturday - 6, Sunday - 0)
 if (date.getDay() >= 1 && date.getDay() <= 5) {
-  var todaysSchedule = schedule[date.getDay() - 1]
+  var queryString = window.location.search;
+
+  var urlParams = new URLSearchParams(queryString);
+
+  var className = urlParams.get('class')
+
+  var todaysSchedule = schedules[className][date.getDay() - 1]
 
   $('#schedule').html('The lessons today are: <ol><li>' + todaysSchedule.join('</li> <li>') + '</li></ol>')
 
